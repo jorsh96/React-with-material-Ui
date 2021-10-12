209626a0a82030a57dd149015283f03c4279df33
jorsh96 <excellence.olatunbosun@decagon.dev>
2021-10-12 20:43:34 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
+import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2e1667',
+    },
+    secondary: {
+      main: '#c7d8ed'
+    },
+    typography : {
+      fontFamily: [
+        'Roboto'
+      ],
+      h4: {
+        fontWeight: 600,
+        fontsize: 28,
+        lineHeight: '2rem',
+      },
+      h5 : {
+        fontWeight: 100,
+        lineHeight: '2rem'
+      }
+    }
+  }
+})
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme = { theme }>
+        
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
